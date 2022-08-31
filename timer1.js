9805d777df9d7170e732b02d034213a982c0b652
nickholmstyle <nick416holm@protonmail.com>
2022-08-31 01:57:03 +0000

    timer, needs to meet one condition still

---
+++ timer1.js
@@ -0,0 +1,44 @@
+// process.stdout.write('\x07');
+
+//INPUT: 
+
+//Edge Cases:
+//No numbers are provided: 
+  //Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
+//An input is a negative number: 
+  //Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+//An input is not a number: 
+  //Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
+
+// input = 0
+
+const numbers = process.argv.slice(2);
+
+const scheduleBeep = function(delay) {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, delay * 1000);
+}
+
+for (let num of numbers) {
+
+  if (Number(isNaN(num))) {
+    return
+  } else {
+    scheduleBeep(num);
+  }
+}
+
+
+
+
+// if (input === isNaN) {
+//   return;
+// }
+
+// if (input < 0) {
+//   return;
+// 
+// if (input === null) {
+//   return;
+// }
